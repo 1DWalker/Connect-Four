c701d01c6bd7030a186d06f360ebe89967d58bc3
1DWalker <alex.j.sheng.yang@gmail.com>
2017-01-14 17:13:17 -0500

    Add files via upload

---
+++ ComputerTwo.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+//computer one makes a move, given the board, time controls, current time, and others. Tell me what parameters you need! Ex: last player's move?
+
+public class ComputerTwo {
+
+    public static int[] makeMove(int[][] board, long[] timeControl, long playerTime) {
+		int[] computerMoveCoordinates = new int[2];
+		Random randomInt = new Random();
+	
+		//Select move to play randomly
+		moveSelection:
+		while (true) {
+			int columnRandom =  randomInt.nextInt(board.length);
+			
+			//Is the column filled?
+			for (int i = board[0].length - 1; i >= 0; i--) {
+				if (board[columnRandom][i] == 0) {
+					computerMoveCoordinates[0] = columnRandom;
+					computerMoveCoordinates[1] = i;
+					break;
+				}
+				
+				if (i == 0) continue moveSelection;
+			}
+			
+			break;
+		}
+		
+		return computerMoveCoordinates;
+    }
+}
+    
+    
